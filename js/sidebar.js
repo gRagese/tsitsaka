4a8e1628f6d34e93e1ecd55ba1f8202c5fc2e7d5
Giorgi Giorgobiani <giorgobianig090@gmail.com>
2024-01-14 14:10:13 +0400

    deleting sidebar stuff

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,8 @@
 const menu_btn = document.querySelector('.header__menu-btn');
 const sidebar = document.querySelector('.sidebar-container');
+const container = document.querySelector('.container')
 
 menu_btn.addEventListener('click', function(e){
     sidebar.classList.add('sidebar-container-active')
+    container.classList.add('container-black')
 })
